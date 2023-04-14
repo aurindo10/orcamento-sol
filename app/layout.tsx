a0489d128272347ca95ace028d2c67e0f72c735a
Aurindo Neto <aurindo15@gmail.com>
2023-04-14 15:54:00 -0300

    ssxas

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import ClientProvider from "../contexts/ClientProvider";
-// import { ClerkProvider } from "@clerk/nextjs/app-beta";
+import { ClerkProvider } from "@clerk/nextjs/app-beta";
 import ResponsiveDrawer from "components/molecules/Drawer";
 import React from "react";
 
@@ -11,17 +11,17 @@ export default function RootLayout({
 }) {
   return (
     <ClientProvider>
-      {/* <ClerkProvider> */}
-      <html lang="pt-br">
-        <head>
-          <title>Sol Orçamentos</title>
-        </head>
-        <body className="h-screen bg-slate-800">
-          <ResponsiveDrawer></ResponsiveDrawer>
-          {children}
-        </body>
-      </html>
-      {/* </ClerkProvider> */}
+      <ClerkProvider>
+        <html lang="pt-br">
+          <head>
+            <title>Sol Orçamentos</title>
+          </head>
+          <body className="h-screen bg-slate-800">
+            <ResponsiveDrawer></ResponsiveDrawer>
+            {children}
+          </body>
+        </html>
+      </ClerkProvider>
     </ClientProvider>
   );
 }
