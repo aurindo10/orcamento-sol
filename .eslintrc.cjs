36ec2e1e731ed61efb6e5d14d420f517d179b67d
Aurindo Neto <aurindo15@gmail.com>
2023-04-16 22:50:01 -0300

    dadas

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,29 @@
-// // eslint-disable-next-line @typescript-eslint/no-var-requires
-// const path = require("path");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
 
-// /** @type {import("eslint").Linter.Config} */
-// const config = {
-//   overrides: [
-//     {
-//       extends: [
-//         "plugin:@typescript-eslint/recommended-requiring-type-checking",
-//       ],
-//       files: ["*.ts", "*.tsx"],
-//       parserOptions: {
-//         project: path.join(__dirname, "tsconfig.json"),
-//       },
-//     },
-//   ],
-//   parser: "@typescript-eslint/parser",
-//   parserOptions: {
-//     project: path.join(__dirname, "tsconfig.json"),
-//   },
-//   plugins: ["@typescript-eslint"],
-//   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
-//   rules: {
-//     "react/no-unescaped-entities": "off",
-//     "@next/next/no-page-custom-font": "off",
-//   },
-// };
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  overrides: [
+    {
+      //   extends: [
+      //     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      //   ],
+      files: ["*.ts", "*.tsx"],
+      parserOptions: {
+        project: path.join(__dirname, "tsconfig.json"),
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: path.join(__dirname, "tsconfig.json"),
+  },
+  plugins: ["@typescript-eslint"],
+  //   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  rules: {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off",
+  },
+};
 
-// module.exports = config;
+module.exports = config;
